755017dba019f0a497379498b0f6cdc3577d1c1c
Derek Li <derekli.3628@gmail.com>
2019-08-27 01:18:18 -0400

    Added feature to have multiple lists

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -1,10 +1,11 @@
- package com.example.myapp;
+package com.example.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -14,18 +15,55 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
- public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-     public static final String EXTRA_MESSAGE = "com.example.myapp.MESSAGE";
-     private String choice = "moo";
-     public HashMap<String, ArrayList<String>> choices = new HashMap<String, ArrayList<String>>();
+    public static final String EXTRA_MESSAGE = "com.example.myapp.MESSAGE";
+    public HashMap<String, ArrayList<String>> choices = new HashMap<String, ArrayList<String>>();
+    public String currentListName = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        StringBuffer text = new StringBuffer();
+
+        /*
+        Simulate loading a list
+        */
+        String newName = "Lunch";
+        String[] newListObjects = new String[]{"Wendy's", "Subway", "Smoke's Poutine", "Jerk Chicken",
+                "Mandarin", "Bang me baby", "PopEyes", "csgo"};
+        ArrayList<String> newList = new ArrayList<>();
+        for (String item : newListObjects) {
+            newList.add(item);
+        }
+
+        String breakfastName = "Breakfast";
+        String[] breakfastListObjects = new String[]{"Cereal", "Pancakes", "Granola bar"};
+        ArrayList<String> breakfastList = new ArrayList<String>();
+        for (String item : breakfastListObjects){
+            breakfastList.add(item);
+        }
+
+
+        choices.put(newName, newList);
+        choices.put(breakfastName, breakfastList);
+
+
+        /*
+        End simulation
+        */
+
+//        this.currentList.add("Wendy's");
+//        this.currentList.add("Subway");
+//        this.currentList.add("Smoke's Poutine");
+//        this.currentList.add("Jerk Chicken");
+//        this.currentList.add("Mandarin");
+//        this.currentList.add("Bang me baby");
+//        this.currentList.add("Popeyes");
+//        this.currentList.add("csgo");
+
 
 //        text.append(" ${"_".repeat(choice.name.length + 2)} \n")
 //          text.append(String.format(" %s \n", "_".repeat(this.choice.length() + 2)));
@@ -37,57 +75,96 @@ import java.util.Map;
 //          text.append("            ||----w |\n");
 //          text.append("            ||     ||\n");
 
-        text.append(String.format(" %s \n", this.repeatString("_", choice.length() + 2)))
-        .append(String.format("/ %s /\n", this.choice))
-        .append(String.format(" %s \n", this.repeatString("-", choice.length() + 2)))
-        .append("    \\   ^__^\n")
-        .append("     \\  (oo)\\_______\n")
-        .append("        (__)\\       )\\/\\\n")
-        .append("            ||----w |\n")
-        .append("            ||     ||\n");
+        if (!choices.isEmpty()){
 
-        TextView cowText = findViewById(R.id.cow);
-        cowText.setText(text.toString());
+        }
+
+        cowsay("moo");
 
         this.addSpinnerItems();
     }
 
-    public void addSpinnerItems(){
-      Spinner spinner = findViewById(R.id.group_choice);
-      List<String> list = new ArrayList<String>();
+    public void addSpinnerItems() {
+        Spinner spinner = findViewById(R.id.group_choice);
+        List<String> list = new ArrayList<String>();
 
-      this.choices.forEach((key,value) -> System.out.println(key + " = " + value));
+        this.choices.forEach((key, value) -> System.out.println(key + " = " + value));
 
-      list.add("list 1");
-      list.add("list 2");
-      ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
-      dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-      spinner.setAdapter(dataAdapter);
+        for (String item : choices.keySet()) {
+            list.add(item);
+        }
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner.setAdapter(dataAdapter);
+        this.setCurrentListName(list.get(0));
     }
 
-//    Called when user taps edit send button
-//    public void editChoices(View view){
-//        Intent intent = new Intent(this, EditActivity.class);
-//        EditText editText = (EditText) findViewById(R.id.edit_btn);
-//        String message = editText.getText().toString();
-//        intent.putExtra(EXTRA_MESSAGE, message);
-//        startActivity(intent);
-//    }
+    public void rollChoices(View view) {
+        Random random = new Random();
+        ArrayList<String> currentList = this.choices.get(this.currentListName);
+        int index = random.nextInt(currentList.size());
+        cowsay(currentList.get(index));
+    }
 
-   /**
-    * Returns a string repeated n times
-    * @param repeat - String to be repeated.
-    * @param n - Number of times to be repeated.
-    * @return - The string repeated n times.
-    */
-    public static String repeatString(String repeat, int n){
-      if (n < 1){
-        return "";
-      }
-      String result = "";
-      for(int i = 0; i < n; i++){
-        result = result + repeat;
-      }
-      return result;
+    public void cowsay(String say) {
+        StringBuffer text = new StringBuffer();
+        text.append(String.format(" %s \n", this.repeatString("_", say.length() + 2)))
+                .append(String.format("/ %s /\n", say))
+                .append(String.format(" %s \n", this.repeatString("-", say.length() + 2)))
+                .append("    \\   ^__^\n")
+                .append("     \\  (oo)\\_______\n")
+                .append("        (__)\\       )\\/\\\n")
+                .append("            ||----w |\n")
+                .append("            ||     ||\n");
+
+        TextView cowText = findViewById(R.id.cow);
+        cowText.setText(text.toString());
+    }
+
+    /**
+     * Called when user taps edit button
+     */
+    public void editChoices(View view) {
+        Intent intent = new Intent(this, EditActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Returns a string repeated n times
+     *
+     * @param repeat - String to be repeated.
+     * @param n      - Number of times to be repeated.
+     * @return - The string repeated n times.
+     */
+    public static String repeatString(String repeat, int n) {
+        if (n < 1) {
+            return "";
+        }
+        String result = "";
+        for (int i = 0; i < n; i++) {
+            result = result + repeat;
+        }
+        return result;
+    }
+
+    public String getCurrentListName(){
+        String x = this.currentListName;
+        return x;
+    }
+
+    public void setCurrentListName(String name){
+        this.currentListName = name;
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        // TODO: change current list upon selection.
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
